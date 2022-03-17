54ae9eef1c827343709c8031a3d39194751be5f1
George Flourentzos <georgeflourentzos@gmail.com>
2022-03-17 14:45:52 +0000

    Added Spot <-> Futures transfer endpoint

---
+++ Huobi.Net/Enums/Futures/FutureTransferType.cs
@@ -0,0 +1,17 @@
+﻿namespace Huobi.Net.Enums.Futures
+{
+    /// <summary>
+    /// Direction of transfer
+    /// </summary>
+    public enum FutureTransferType
+    {
+        /// <summary>
+        /// Spot to Futures
+        /// </summary>
+        SpotToFutures,
+        /// <summary>
+        /// Futures to Spot
+        /// </summary>
+        FuturesToSpot
+    }
+}
