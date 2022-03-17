08cbb39e0c66cdc152334df2a8a807090fab4dea
George Flourentzos <georgeflourentzos@gmail.com>
2022-03-17 15:52:15 +0000

    Added Spot <-> USDT-Swap transfer endpoint

---
+++ Huobi.Net/Converters/Futures/UsdtSwapTransferTypeConverter.cs
@@ -0,0 +1,19 @@
+﻿using CryptoExchange.Net.Converters;
+using Huobi.Net.Enums.Futures;
+using System.Collections.Generic;
+
+namespace Huobi.Net.Converters.Futures
+{
+    internal class UsdtSwapTransferTypeConverter : BaseConverter<UsdtSwapTransferType>
+    {
+        public UsdtSwapTransferTypeConverter() : this(true) { }
+
+        public UsdtSwapTransferTypeConverter(bool useQuotes) : base(useQuotes) { }
+
+        protected override List<KeyValuePair<UsdtSwapTransferType, string>> Mapping => new List<KeyValuePair<UsdtSwapTransferType, string>>
+        {
+            new KeyValuePair<UsdtSwapTransferType, string>(UsdtSwapTransferType.Spot, "spot"),
+            new KeyValuePair<UsdtSwapTransferType, string>(UsdtSwapTransferType.LinearSwap, "linear-swap")
+        };
+    }
+}
---
+++ Huobi.Net/Enums/Futures/UsdtSwapTransferType.cs
@@ -0,0 +1,17 @@
+﻿namespace Huobi.Net.Enums.Futures
+{
+    /// <summary>
+    /// Transfer type
+    /// </summary>
+    public enum UsdtSwapTransferType
+    {
+        /// <summary>
+        /// Spot
+        /// </summary>
+        Spot,
+        /// <summary>
+        /// Linear Swap
+        /// </summary>
+        LinearSwap
+    }
+}
